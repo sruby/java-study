f18b81ead89e3d1a28567b1639089add31adc614
sruby <hisruby001@gmail.com>
2020-08-04 16:51:04 +0800

    radix sort

--- src/main/java/io/github/sruby/arithmetic/sort/RadixSort.java
+++ src/main/java/io/github/sruby/arithmetic/sort/RadixSort.java
@@ -5,8 +5,52 @@ package io.github.sruby.arithmetic.sort;
  */
 public class RadixSort {
     public int[] sort(int[] nums){
+        int max = nums[0];
+        for(int i = 1;i<nums.length; i++){
+            if (max < nums[i]){
+                max = nums[i];
+            }
+        }
 
+        int percent = 1;
+        while (max/percent > 0 ){
+            sortByPercent(nums,percent);
+            percent *= 10;
+        }
 
         return nums;
+    }
+
+    private void sortByPercent(int[] nums, int percent) {
+
+        /**
+         * count per number
+         */
+        int[] countArray = new int[10];
+        for (int i = 0 ; i < nums.length; i++){
+            int numOnCurrentPercent = nums[i] / percent % 10;
+            countArray[numOnCurrentPercent]++;
+        }
+
+        /**
+         *Calculate the position after sorting
+         */
+        for (int i =1; i < 10;i++){
+            countArray[i] += countArray[i-1];
+        }
+
+        /**
+         *store new array after sorting
+         */
+        int[] newArray = new int[nums.length];
+        //must sort from tail
+        for (int i = nums.length - 1; i >= 0; i--) {
+             newArray[countArray[nums[i]/percent%10]-1] = nums[i];
+             countArray[nums[i]/percent%10] --;
+        }
+
+        for (int i = 0 ;i < nums.length; i++){
+            nums[i] = newArray[i];
+        }
     }
 }
