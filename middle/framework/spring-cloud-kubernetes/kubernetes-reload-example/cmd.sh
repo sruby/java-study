64cc382cd4f14ba869e2b50f197d344cb0806e7f
sruby <15009808+sruby@users.noreply.github.com>
2021-08-30 20:52:15 +0800

    spring cloud k8s

--- middle/framework/spring-cloud-kubernetes/kubernetes-reload-example/cmd.sh
+++ middle/framework/spring-cloud-kubernetes/kubernetes-reload-example/cmd.sh
@@ -1,11 +1,17 @@
+#load image to minikube from docker
+minikube image load kubernetes/spring-cloud-kubernetes-example-reload:0.0.4
+# edit deployment for add serviceaccount: serviceAccountName: config-reader
+kubectl edit deployment spring-cloud-reload
+
 kubectl create configmap reload-example application.properties
 
 #create configmap
 kubectl create configmap reload-example --from-file application.properties --from-file ./src/main/resources/application-dev.properties
 
-#replace configmap
+#replace configmap from one file
 kubectl create configmap reload-example --from-file ./application.properties -o yaml --dry-run | kubectl replace -f -
 
-#replace configmap
-kubectl create configmap reload-example --from-file ./application.properties --from-file ./src/main/resources/application-dev.properties -o yaml --dry-run | kubectl replace -f -
+#replace configmap from multi file
+kubectl create configmap reload-example --from-file ./application.properties --from-file ./src/main/resources/application-kubernetes.properties -o yaml --dry-run | kubectl replace -f -
+
 
--- middle/framework/spring-cloud-kubernetes/kubernetes-reload-example/src/main/java/org/springframework/cloud/kubernetes/examples/MyBean.java
+++ middle/framework/spring-cloud-kubernetes/kubernetes-reload-example/src/main/java/org/springframework/cloud/kubernetes/examples/MyBean.java
@@ -17,6 +17,7 @@
 package org.springframework.cloud.kubernetes.examples;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -29,10 +30,14 @@ public class MyBean {
 	@Autowired
 	private DummyConfig dummyConfig;
 
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
+
 	@Scheduled(fixedDelay = 5000)
 	public void hello() {
 		System.out.println("The first message is: " + this.myConfig.getMessage());
 		System.out.println("The other message is: " + this.dummyConfig.getMessage());
+		System.out.println("db url:" + dbUrl);
 	}
 
 }
