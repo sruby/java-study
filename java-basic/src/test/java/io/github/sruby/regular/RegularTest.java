e380abc5f0685625751f6024df76efa18e52630b
sruby <15009808+sruby@users.noreply.github.com>
2021-09-18 19:49:45 +0800

    no message

---
+++ java-basic/src/test/java/io/github/sruby/regular/RegularTest.java
@@ -0,0 +1,15 @@
+package io.github.sruby.regular;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @description: regular test
+ * @author: sruby
+ * @create: 2021-09-17 16:46
+ */
+public class RegularTest {
+    @Test
+    public void test(){
+        
+    }
+}
