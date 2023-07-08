0904097083f21800817dcd359b2f689965643304
sruby <15009808+sruby@users.noreply.github.com>
2023-07-08 22:33:07 +0800

    list node

--- advanced/arithmetic/arithmetic/src/main/java/io/github/sruby/arithmetic/listnode/MergeTwoSortedLists.java
+++ advanced/arithmetic/arithmetic/src/main/java/io/github/sruby/arithmetic/listnode/MergeTwoSortedLists.java
@@ -23,21 +23,25 @@ public class MergeTwoSortedLists {
         ListNode p2 = list2;
 
         while ( p1 !=null && p2 !=null){
+            // 比较 p1 和 p2 两个指针
+            // 将值较小的的节点接到 p 指针
             if (p1.val > p2.val){
                 p.next = p2;
                 p2 = p2.next;
-
-                if (p2 == null){
-                    p.next = p1;
-                }
             }else {
                 p.next = p1;
                 p1 = p1.next;
-                if (p1 == null){
-                    p.next = p2;
-                }
             }
+            // p 指针不断前进
             p = p.next;
+        }
+
+        if (p1 == null){
+            p.next = p2;
+        }
+
+        if (p2 == null){
+            p.next = p1;
         }
 
         return dummy.next;
