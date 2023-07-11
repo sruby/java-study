008cb519dd9f75bbf417652d18812316d6acf257
sruby <15009808+sruby@users.noreply.github.com>
2023-07-11 15:47:07 +0800

    partition list

---
+++ advanced/arithmetic/arithmetic/src/main/java/io/github/sruby/arithmetic/listnode/MergeKSortedLists.java
@@ -0,0 +1,13 @@
+package io.github.sruby.arithmetic.listnode;
+
+/**
+ * 23. Merge k Sorted Lists
+ *
+ * @author Sruby
+ * @date 11/7/2023 15:46
+ */
+public class MergeKSortedLists {
+    public ListNode mergeKLists(ListNode[] lists) {
+        return null;
+    }
+}
--- advanced/arithmetic/arithmetic/src/main/java/io/github/sruby/arithmetic/listnode/PartitionList.java
+++ advanced/arithmetic/arithmetic/src/main/java/io/github/sruby/arithmetic/listnode/PartitionList.java
@@ -11,12 +11,14 @@ public class PartitionList {
         if (head == null){
             return null;
         }
-
+        // 存放小于 x 的链表的虚拟头结点
         ListNode firstDummy = new ListNode(-1);
+        // 存放大于等于 x 的链表的虚拟头结点
         ListNode lastDummy = new ListNode(-1);
+        // point指针负责生成结果链表
         ListNode firstPoint = firstDummy;
         ListNode lastPoint = lastDummy;
-
+        // head 负责遍历原链表，类似合并两个有序链表的逻辑
         while (head != null){
             if (head.val < x){
                 firstPoint.next = head;
@@ -27,9 +29,12 @@ public class PartitionList {
             }
             head = head.next;
         }
-        if (lastDummy.next !=null){
-            lastPoint.next = firstDummy.next;
-        }
+        //lastPoint需要把 next 设置为 null（断开原有链表的指针），避免最后带上原有链表的数据
+        lastPoint.next = null;
+
+        //因为最终 firstPoint 的 next 会连接到lastDummy,所以不需要断开原有的连接
+        // 连接两个链表
+        firstPoint.next =  lastDummy.next;
 
         return firstDummy.next;
     }
