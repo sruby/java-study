1ea91d83a9669d31d995f7e631d5e1bff97b0167
sruby <15009808+sruby@users.noreply.github.com>
2023-07-23 10:13:45 +0800

    ApiClientProxy

---
+++ advanced/interview/interview-examination/src/main/java/io/github/sruby/interview/examination/vivo/fintech/solution3/ApiClient.java
@@ -0,0 +1,11 @@
+package io.github.sruby.interview.examination.vivo.fintech.solution3;
+
+/**
+ * ApiClient
+ *
+ * @author Sruby
+ * @date 7/23/2023 10:06 AM
+ */
+public interface ApiClient {
+    public String call();
+}
