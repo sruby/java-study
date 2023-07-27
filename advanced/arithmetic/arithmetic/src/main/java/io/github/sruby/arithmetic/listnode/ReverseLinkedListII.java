50b4c413d9f71eb3838a28eeafe0a91702f4081b
sruby <15009808+sruby@users.noreply.github.com>
2023-07-27 14:28:48 +0800

    ReverseLinkedListII

--- advanced/arithmetic/arithmetic/src/main/java/io/github/sruby/arithmetic/listnode/ReverseLinkedListII.java
+++ advanced/arithmetic/arithmetic/src/main/java/io/github/sruby/arithmetic/listnode/ReverseLinkedListII.java
@@ -7,11 +7,51 @@ package io.github.sruby.arithmetic.listnode;
  * @date 7/15/2023 6:06 PM
  */
 public class ReverseLinkedListII {
+
+    /**
+     * reverse between
+     * @param head
+     * @param left
+     * @param right
+     * @return
+     */
     public ListNode reverseBetween(ListNode head, int left, int right) {
         /**
-         * 移动到left
+         * 移动 left-1，找到 pre 前驱节点
+         * 移动 right,找到successor 后继节点
          */
+        ListNode dump = new ListNode(-1);
+        dump.next = head;
+        ListNode pre = head;
+        ListNode successor = null;
+
+        for (int i = 0; i < right ; i++){
+            if (i < left -2){
+                pre = pre.next;
+            }else if(i == left - 2){
+                successor = pre;
+            }else {
+                successor = successor.next;
+            }
+        }
+
+        /**
+         * 反转中间部分
+         */
+        ListNode newHead = pre.next;
+        reverseList(newHead);
 
         return head;
+    }
+
+    private ListNode reverseList(ListNode newHead) {
+        ListNode p = newHead;
+        while (p.next !=null){
+            ListNode temp = p.next;
+            p.next.next = p;
+            p.next = null;
+            p = temp;
+        }
+        return p.next;
     }
 }
