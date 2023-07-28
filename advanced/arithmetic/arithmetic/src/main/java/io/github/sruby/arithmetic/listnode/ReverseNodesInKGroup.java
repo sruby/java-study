639150bca57a72d829467738941d4d5d1f3e3397
sruby <15009808+sruby@users.noreply.github.com>
2023-07-28 20:11:37 +0800

    92. Reverse Linked List II

---
+++ advanced/arithmetic/arithmetic/src/main/java/io/github/sruby/arithmetic/listnode/ReverseNodesInKGroup.java
@@ -0,0 +1,13 @@
+package io.github.sruby.arithmetic.listnode;
+
+/**
+ * 25. Reverse Nodes in k-Group
+ *
+ * @author Sruby
+ * @date 7/28/2023 8:11 PM
+ */
+public class ReverseNodesInKGroup {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        return null;
+    }
+}
