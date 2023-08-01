d3eaee6789025db5a71d37407479f14059a4642d
sruby <15009808+sruby@users.noreply.github.com>
2023-08-01 17:28:47 +0800

    Reverse Nodes in k-Group

---
+++ advanced/arithmetic/arithmetic/src/main/java/io/github/sruby/arithmetic/listnode/ReverseNodesinkGroup.java
@@ -0,0 +1,53 @@
+package io.github.sruby.arithmetic.listnode;
+
+/**
+ * Reverse Nodes in k-Group
+ *
+ * @author Sruby
+ * @date 1/8/2023 16:55
+ */
+public class ReverseNodesinkGroup {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null){
+            return null;
+        }
+        //b 指向的是每部分需要反转的链表的后继节点（也是下一部分需要反转的链表的头节点）
+        ListNode a = head,b = head;
+
+
+        for( int i = 0; i< k; i++){
+            // 不足 k 个，不需要反转，base case
+            if (b == null){
+                return a;
+            }
+            b = b.next;
+        }
+        // 反转前 k 个元素
+        ListNode newHead = reverse(a,b);
+        System.out.println("newHead:"+newHead.val);
+        //递归反转后的链表连接
+        a.next = reverseKGroup(b,k);
+
+        return newHead;
+    }
+
+    /**
+     * 反转 [a，b}前闭后开
+     * @param a
+     * @param b
+     * @return
+     */
+    public ListNode reverse(ListNode a, ListNode b){
+
+        ListNode pre = new ListNode(-1);
+        ListNode cur = a;
+        while (cur != b){
+            ListNode temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+//            System.out.println("cur:"+cur.val+",b:"+b.val);
+        }
+        return pre;
+    }
+}
---
+++ advanced/arithmetic/arithmetic/src/test/java/io/github/sruby/arithmetic/listnode/ReverseNodesinkGroupTest.java
@@ -0,0 +1,30 @@
+package io.github.sruby.arithmetic.listnode;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * test
+ *
+ * @author Sruby
+ * @date 1/8/2023 17:10
+ */
+class ReverseNodesinkGroupTest {
+    private ReverseNodesinkGroup reverseNodesinkGroup = new ReverseNodesinkGroup();
+    @Test
+    public void test() {
+        ListNode listNode = new ListNode(1);
+        listNode.next = new ListNode(2);
+        listNode.next.next = new ListNode(3);
+        listNode.next.next.next = new ListNode(4);
+        listNode.next.next.next.next = new ListNode(5);
+        ListNode reverseKGroup = reverseNodesinkGroup.reverseKGroup(listNode, 2);
+
+        while (reverseKGroup !=null){
+            System.out.println(reverseKGroup.val);
+            reverseKGroup = reverseKGroup.next;
+        }
+
+
+    }
+
+}
