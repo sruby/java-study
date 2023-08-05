03631885468ccadcd1241fe31f18d0a71d1d0b22
sruby <15009808+sruby@users.noreply.github.com>
2023-08-05 23:34:24 +0800

    flatten Binary Tree to Linked List

--- advanced/arithmetic/arithmetic/src/main/java/io/github/sruby/arithmetic/binarytree/FlattenBinaryTreeToLinkedList.java
+++ advanced/arithmetic/arithmetic/src/main/java/io/github/sruby/arithmetic/binarytree/FlattenBinaryTreeToLinkedList.java
@@ -12,17 +12,27 @@ public class FlattenBinaryTreeToLinkedList {
             return ;
         }
 
-        if (root.left == null || root.right == null){
-            return ;
-        }
-
-        TreeNode temp = root.right;
-        root.right = root.left;
-        root.left = null;
-        root.right.right = temp;
-        root.right.left = null;
-
         flatten(root.left);
         flatten(root.right);
+
+        TreeNode oldLeft = root.left;
+        TreeNode oldRight = root.right;
+
+        /**
+         * 左节点变成又节点，左节点设置为null
+         */
+        root.right = oldLeft;
+        root.left = null;
+
+        /**
+         * 原有右节点接到新的右节点末尾
+         *
+         */
+        TreeNode point = root.right;
+        while (point.right !=null){ //point.right !=null 而不是 point !=null,叶子节点的right是null，会导致空指针
+            point = point.right;
+        }
+        point.right = oldRight;
+
     }
 }
