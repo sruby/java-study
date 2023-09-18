b897bc07d8d499dabcdde040eab4447839c08958
sruby <15009808+sruby@users.noreply.github.com>
2023-09-18 23:59:33 +0800

    input & outpug

---
+++ advanced/python/python-study/input_and_output.py
@@ -0,0 +1,10 @@
+name = input("your name:")
+gender = input("your are a boy?(y/n):")
+
+welcome_str = 'Welcome to the matrix {prefix} {name}.'
+welcome_dic = {
+    'prefix':'Mr.' if gender == 'y' else 'Mrs' ,
+    'name':name
+}
+
+print(welcome_str.format(**welcome_dic))
