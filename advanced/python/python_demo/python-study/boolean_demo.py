1b296fb5f4484b2b682aefc67fcbf1de45e5d7be
sruby <15009808+sruby@users.noreply.github.com>
2024-01-24 16:26:23 +0800

    get data from tushare

---
+++ advanced/python/python_demo/python-study/boolean_demo.py
@@ -0,0 +1,8 @@
+a = 3
+b = 2
+# c = False
+c = True
+if a > b and not c:
+    print("a is greater than b")
+else:
+    print("else")
--- advanced/python/python_demo/tushare_demo/get_history_data.py
+++ advanced/python/python_demo/tushare_demo/get_history_data.py
@@ -1,3 +1,18 @@
 import tushare as ts
+from dotenv import load_dotenv
+import os
 
-ts.get_hist_data('600848')
+load_dotenv()  # This is the crucial part
+token = os.getenv('TOKEN')
+ts.set_token(token)
+pro = ts.pro_api()
+
+#查询当前所有正常上市交易的股票列表
+# data = pro.stock_basic(exchange='', list_status='L', fields='ts_code,symbol,name,area,industry,list_date')
+# print(data)
+
+df = pro.daily(ts_code='000001.SZ', start_date='20180701', end_date='20180718')
+print(df)
+#多个股票
+df = pro.daily(ts_code='000001.SZ,600000.SH', start_date='20180701', end_date='20180718')
+print(df)
