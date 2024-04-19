acd034a3247351fa92141b8f0923b323d15ae53d
sruby <15009808+sruby@users.noreply.github.com>
2024-04-19 11:16:37 +0800

    Daily auto-commit

--- advanced/os/windows/daily_commit.bat
+++ advanced/os/windows/daily_commit.bat
@@ -1,5 +1,9 @@
 @echo off
 cd /d C:\Users\hisru\Documents\GitHub\juejin_demo
+:: 设置 Git 代理
+git config --local http.proxy 127.0.0.1:7890
+git config --local https.proxy 127.0.0.1:7890
+
 git add .
 git commit -m "Daily auto-commit"
 git push
@@ -8,3 +12,7 @@ cd /d C:\Users\hisru\Documents\GitHub\java-study
 git add .
 git commit -m "Daily auto-commit"
 git push
+
+:: 取消设置 Git 代理
+git config --local --unset http.proxy
+git config --local --unset https.proxy
