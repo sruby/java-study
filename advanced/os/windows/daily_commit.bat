a2ca205424ee219adee93fb7fcc0c88b7a6cd808
sruby <15009808+sruby@users.noreply.github.com>
2024-04-19 11:11:55 +0800

    Daily auto-commit

---
+++ advanced/os/windows/daily_commit.bat
@@ -0,0 +1,10 @@
+@echo off
+cd /d C:\Users\hisru\Documents\GitHub\juejin_demo
+git add .
+git commit -m "Daily auto-commit"
+git push
+
+cd /d C:\Users\hisru\Documents\GitHub\java-study
+git add .
+git commit -m "Daily auto-commit"
+git push
