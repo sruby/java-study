6ed1837eb0610f33fea927cc3570e02e836b77c9
sruby <15009808+sruby@users.noreply.github.com>
2024-06-07 18:30:05 +0800

    Daily auto-commit

--- advanced/os/windows/daily_commit.bat
+++ advanced/os/windows/daily_commit.bat
@@ -15,3 +15,11 @@ git config --local https.proxy 127.0.0.1:7890
 git add .
 git commit -m "Daily auto-commit"
 git push
+
+cd /d C:\Users\hisru\Documents\code\python_project
+:: 设置 Git 代理
+git config --local http.proxy 127.0.0.1:7890
+git config --local https.proxy 127.0.0.1:7890
+git add .
+git commit -m "Daily auto-commit"
+git push
