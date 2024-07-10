3df02e131f49a1dc4d4bbea798268f36abc587e7
sruby <15009808+sruby@users.noreply.github.com>
2024-07-10 18:30:04 +0800

    Daily auto-commit

--- advanced/os/windows/daily_commit.bat
+++ advanced/os/windows/daily_commit.bat
@@ -23,3 +23,11 @@ git config --local https.proxy 127.0.0.1:7890
 git add .
 git commit -m "Daily auto-commit"
 git push
+
+cd /d C:\Users\hisru\Documents\GitHub\strategy_project
+:: 设置 Git 代理
+git config --local http.proxy 127.0.0.1:7890
+git config --local https.proxy 127.0.0.1:7890
+git add .
+git commit -m "Daily auto-commit"
+git push
