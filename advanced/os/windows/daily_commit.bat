4aa44d92da663ea43e34419b9e4bbb36d00de0ae
sruby <15009808+sruby@users.noreply.github.com>
2024-07-23 18:30:04 +0800

    Daily auto-commit

--- advanced/os/windows/daily_commit.bat
+++ advanced/os/windows/daily_commit.bat
@@ -32,6 +32,14 @@ git add .
 git commit -m "Daily auto-commit"
 git push
 
+cd /d C:\Users\hisru\Documents\GitHub\xrisk_extend
+:: 设置 Git 代理
+git config --local http.proxy 127.0.0.1:7890
+git config --local https.proxy 127.0.0.1:7890
+git add .
+git commit -m "Daily auto-commit"
+git push
+
 cd /d C:\Users\hisru\Documents\code\zs-duij
 :: 设置 Git 代理
 git add .
